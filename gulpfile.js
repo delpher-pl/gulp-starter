a6eebd242c98afa8ddefcaba46c74c4082010469
delpher.pl <delpher00@wp.pl>
2018-08-06 23:31:12 +0200

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
 const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+
+const path = {};
+
+
+gulp.task('html', function(){});
+gulp.task('sass', function(){});
+gulp.task('cssMin', function(){});
+gulp.task('jsImportLibs', function(){});
+gulp.task('js', function(){});
+gulp.task('watch', function(){});
+gulp.task('default', function(){});
+gulp.task('build', function(){});
+
+
+gulp.task('serve', function(){
+  browserSync.init({
+      server: "./dist/",
+      open: false
+  });
+});
